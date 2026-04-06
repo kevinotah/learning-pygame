3e9ad33a33a210b03ecb882b1fbac1649ae63995
Kevin Otah <kevin.ogbusuo@epita.fr>
2026-04-07 00:48:06 +0200

    Added icon

--- basics.py
+++ basics.py
@@ -2,12 +2,21 @@ import pygame
 
 pygame.init()
 display = pygame.display.set_mode((1000, 500))
-pygame.display.set_caption("Basics", "bscs")
+
+icon = pygame.image.load("epita.png")
+icon = pygame.transform.scale(icon, (32, 32))
+
+pygame.display.set_icon(icon)
+pygame.display.set_caption("Basics")
+
+clock = pygame.time.Clock()
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
+        
     pygame.display.update()
+    clock.tick(60)
+pygame.quit()
