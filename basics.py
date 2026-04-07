f85d4085cd3070fb306bb7a3f79e83dd117a8b78
Kevin Otah <kevin.ogbusuo@epita.fr>
2026-04-07 04:40:13 +0200

    Added monster

--- basics.py
+++ basics.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, os
 
 pygame.init()
 display = pygame.display.set_mode((1000, 500))
@@ -11,21 +11,40 @@ pygame.display.set_caption("Basics")
 
 clock = pygame.time.Clock()
 
-sky_surface = pygame.image.load('sky.png')
+font = pygame.font.Font("retrovibes.ttf", 100)
 
+sky_surface = pygame.image.load('sky.png')
 ground_surface = pygame.image.load('pavement.png')
-ground_surface = pygame.transform.scale(ground_surface, (1200, 600))
-center = ground_surface.get_rect(center = (500, 250))
+text_surface = font.render("Basics", True, 'black')
+rect = text_surface.get_rect(center = (500, 250))
+
+folder = "monster"
+monster_frames = []
+for file in sorted(os.listdir(folder)):
+    monster = pygame.image.load(f"{folder}/{file}")
+    monster = pygame.transform.scale(monster, (300, 300))
+    monster_frames.append(monster)
 
 running = True
+i = 0
+x = 700
 while running:
+    if i >= len(monster_frames) - 1:
+        i = 0
+    
+    clock.tick(60)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
     display.blit(sky_surface, (-1, -1))
-    display.blit(ground_surface, center)        
+    display.blit(ground_surface, (-20, -50))
+    display.blit(text_surface, rect)  
+    display.blit(monster_frames[i], (x, 200))
+    i += 1
+    x -= 5
         
     pygame.display.update()
-    clock.tick(60)
+    
 pygame.quit()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,8 @@
-ground_surface
+import os, pygame
+folder = "monster"
+monster_frames = []
+for file in sorted(os.listdir(folder)):
+    monster_frames.append(pygame.image.load(f"{folder}/{file}"))
+
+for img in monster_frames:
+    print(img)
